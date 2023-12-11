2aa531a1961d95150a6bc41f03537ab93157de91
Adrian <adrianbueno095@gmail.com>
2023-12-11 17:40:18 +0100

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { format } from "date-fns";
 import "./App.css";
 import IdCard from "./components /IdCard";
 
+
 function App() {
+  let birthInfo = format(new Date(1992, 7, 14), 'MM/dd/yyyy')
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
-      <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={new Date("1992-07-14").toString} picture="https://randomuser.me/api/portraits/men/44.jpg" />
+      <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={birthInfo} picture="https://randomuser.me/api/portraits/men/44.jpg" />
 
     </div>
   );
