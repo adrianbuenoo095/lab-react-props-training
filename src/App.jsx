bf5cc75786b98aff596d043c94060c07f57d42b6
Adrian <adrianbueno095@gmail.com>
2023-12-11 17:32:50 +0100

    Fix issue with card not displaying

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
-      <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={new Date("1992-07-14")} picture="https://randomuser.me/api/portraits/men/44.jpg" />
+      <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={new Date("1992-07-14").toString} picture="https://randomuser.me/api/portraits/men/44.jpg" />
 
     </div>
   );
--- src/components /IdCard.jsx
+++ src/components /IdCard.jsx
@@ -2,17 +2,23 @@ const IdCard = ({ lastName, firstName, gender, height, birth, picture }) => {
     return (
         <div>
             <img
-                className="card"
+                className="person"
                 src={picture}
+                alt={firstName}
+                width={70}
+                height={70}
+
             />
-            <p>FirstName:{firstName}</p>
-            <p>LastName:{lastName}</p>
-            <h1>gender:{gender}</h1>
-            <p>height:{height}</p>
-            <h1>birth:{birth}</h1>
+
+            <ul>
+                <li><b>FirstName:</b>{firstName} </li>
+                <li><b>lastName:</b>{lastName} </li>
+                <li><b>Gender:</b>{gender} </li>
+                <li><b>Height:</b>{height} </li>
+                <li><b>Birth:</b>{birth} </li>
+            </ul>
         </div>
     )
-
 }
 
 export default IdCard;
