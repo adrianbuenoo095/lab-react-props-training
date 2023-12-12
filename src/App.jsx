6a09aaaed48cc84b10226a9d1dd1a19c6066c3b1
Adrian <adrianbueno095@gmail.com>
2023-12-12 10:33:49 +0100

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
       <h1> LAB | React Training</h1>
       <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={format(new Date(1992, 7, 14), 'MM/dd/yyyy')} picture="https://randomuser.me/api/portraits/men/44.jpg" />
       <IdCard firstName='Delores' lastName='Obrien' gender='female' height={172} birth={format(new Date(1988, 5, 11), 'MM/dd/yyyy')} picture="https://randomuser.me/api/portraits/women/44.jpg" />
-
     </div>
   );
 }
