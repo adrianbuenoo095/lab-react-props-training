a6fa852c6e3171d0c2c1fc656313dff31a74db93
Adrian <adrianbueno095@gmail.com>
2023-12-12 10:33:18 +0100

    not done yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import IdCard from "./components /IdCard";
 
 
-function App() {
-  let birthInfo = format(new Date(1992, 7, 14), 'MM/dd/yyyy')
+const App = () => {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
-      <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={birthInfo} picture="https://randomuser.me/api/portraits/men/44.jpg" />
+      <IdCard firstName='John' lastName='Doe' gender='male' height={178} birth={format(new Date(1992, 7, 14), 'MM/dd/yyyy')} picture="https://randomuser.me/api/portraits/men/44.jpg" />
+      <IdCard firstName='Delores' lastName='Obrien' gender='female' height={172} birth={format(new Date(1988, 5, 11), 'MM/dd/yyyy')} picture="https://randomuser.me/api/portraits/women/44.jpg" />
 
     </div>
   );
--- src/components /IdCard.jsx
+++ src/components /IdCard.jsx
@@ -1,21 +1,23 @@
+import classes from "./idcard.module.css";
+
 const IdCard = ({ lastName, firstName, gender, height, birth, picture }) => {
     return (
-        <div>
-            <img
-                className="person"
-                src={picture}
-                alt={firstName}
-                width={70}
-                height={70}
-            />
-            <ul>
-                <li><b>FirstName:</b>{firstName} </li>
-                <li><b>lastName:</b>{lastName} </li>
-                <li><b>Gender:</b>{gender} </li>
-                <li><b>Height:</b>{height} </li>
-                <li><b>Birth:</b>{birth} </li>
-            </ul>
-        </div>
+        <div className={classes.mainContent}>
+            <div>
+                <img
+                    className={classes.img}
+                    src={picture}
+                    alt={firstName}
+                />
+            </div>
+            <div >
+                <p><span className={classes.boldText}>FirstName:</span>{firstName}</p>
+                <p><span className={classes.boldText}>lastName:</span>{lastName}</p>
+                <p><span className={classes.boldText}>Gender:</span> {gender}</p>
+                <p><span className={classes.boldText}>Height:</span> {height}</p>
+                <p><span className={classes.boldText}>Birth:</span>{birth}</p>
+            </div >
+        </div >
     )
 }
 
